066d93966efc4adc2d45fc0efaef62e8afb16142
Habiba MANHY ALI <Habiba.manhy@gmail.com>
2026-09-17 17:28:39 +0200

    init

---
+++ mon-api/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.get("/test/:id", (req, res) => {
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+})
+
+app.listen(3000, () => console.log("http://localhost:3000"))
---
+++ my-app/src/components/button.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react" ; 
+
+const Title = () => {
+     const [count, setCount] = useState(0);
+     const [titre, setTitre] = useState("Hello World!");
+
+    return (
+        <div>
+        <h1>{titre}</h1>
+        <p>Ceci est un composant React simple.</p>
+        <p>{count}</p>
+
+        <button onClick={() => {
+            setCount(count + 1)
+        }}>click sur moi</button>
+
+        <button onClick={() => {
+            setTitre("Le titre a changé")
+        }}>click pour changer le titre</button>
+
+    </div>
+    )
+}
+
+export default Title; 
