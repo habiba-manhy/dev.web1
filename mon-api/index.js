77a8cab1a0afe6ad5a2d143ce5c87b8554257131
Habiba MANHY ALI <Habiba.manhy@gmail.com>
2026-09-21 16:56:12 +0200

    cors

--- mon-api/index.js
+++ mon-api/index.js
@@ -1,11 +1,19 @@
 import express from "express"
 
+function cors(req, res, next){
+     res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Methodes', '*')
+    res.set('Access-Control-Allow-Headers', 'Content-Type, x-api-key')
+
+    next()
+}
+
 const app = express()
 app.use(express.json())
 
 app.get("/test/:id", (req, res) => {
 
-fetch("https://jsonplaceholder.typicode.com/posts/1")
+fetch("https://dev-web1.onrender.com")
     .then(res => res.json())
     .then(data => response.json(data));
 
